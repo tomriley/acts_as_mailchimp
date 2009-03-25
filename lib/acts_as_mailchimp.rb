b7883d3e62e8bee908ce67dc0a7467aca9e1933b
Brian Getting <brian@terra-firma-design.com>
2009-03-25 00:02:13 -0700

    Updating to use the Hominid GemPlugin

--- lib/acts_as_mailchimp.rb
+++ lib/acts_as_mailchimp.rb
@@ -1,16 +1,11 @@
 require 'xmlrpc/client'
+require 'hominid'
 module Terra
   module Acts #:nodoc:
     module MailChimp #:nodoc:
-      class MailChimpConfigError < StandardError; end
-      class MailChimpConnectError < StandardError; end
 
       def self.included(base)
         base.extend ClassMethods
-        mattr_reader :monkeybrains
-        begin
-          @@monkeybrains = YAML.load(File.open("#{RAILS_ROOT}/config/monkeybrains.yml"))[RAILS_ENV].symbolize_keys        
-        end
       end
 
       module ClassMethods
@@ -33,82 +28,46 @@ module Terra
       end
 
       module InstanceMethods
+        
         # Add a user to a MailChimp mailing list
-        def add_to_mailchimp(list_name, double_opt = false)
-          apikey ||= chimpLogin(monkeybrains[:username], monkeybrains[:password])
-          list_id ||= find_mailing_list(apikey, list_name)
+        def add_to_mailchimp(list_name)
+          hominid ||= Hominid.new
+          list_id ||= find_mailing_list(list_name)
           vars = {}
           vars.merge!({"FNAME" => self[fname_column]}) if self.has_attribute?(fname_column)
           vars.merge!({"LNAME" => self[lname_column]}) if self.has_attribute?(lname_column)
-          chimpSubscribe(apikey, list_id["id"], self[email_column], vars, self.class.type_column, double_opt)
-        rescue XMLRPC::FaultException
+          hominid.subscribe(list_id["id"], self[email_column], vars, self[type_column])
+        rescue
+          false
         end
         
         # Remove a user from a MailChimp mailing list
         def remove_from_mailchimp(list_name)
-          apikey ||= chimpLogin(monkeybrains[:username], monkeybrains[:password])
-          list_id ||= find_mailing_list(apikey, list_name)
-          chimpUnsubscribe(apikey, list_id["id"], self[email_column])
-        rescue XMLRPC::FaultException
+          hominid ||= Hominid.new
+          list_id || find_mailing_list(list_name)
+          hominid.unsubscribe(list_id["id"], self[email_column])
+        rescue
+          false
         end
         
         # Update user information at MailChimp
-        def update_mailchimp(list_name, old_email = self[email_column])
-          apikey ||= chimpLogin(monkeybrains[:username], monkeybrains[:password])
-          list_id ||= find_mailing_list(apikey, list_name)
+        def update_mailchimp(list_name, current_email = self[email_column])
+          hominid ||= Hominid.new
+          list_id || find_mailing_list(list_name)
           vars = {}
           vars.merge!({"FNAME" => self[fname_column]}) if self.has_attribute?(fname_column)
           vars.merge!({"LNAME" => self[lname_column]}) if self.has_attribute?(lname_column)
           vars.merge!({"EMAIL" => self[email_column]})
-          chimpUpdate(apikey, list_id["id"], old_email, vars, self[type_column], true)
-        rescue XMLRPC::FaultException
-        end
-        
-        # Log in to MailChimp
-        def chimpLogin(username, password)
-          raise MailChimpConfigError("Please provide a valid username and password") if (username.nil? || password.nil?) 
-          chimpAPI ||= XMLRPC::Client.new2("http://api.mailchimp.com/1.1/")
-          chimpAPI.call("login", username, password)
-        end
-        
-        # Subscribe the provided email to a list
-        def chimpSubscribe(apikey, list_id, email, merge_vars, content_type = 'html', double_opt = true)
-          raise_errors(apikey, list_id)
-          chimpAPI ||= XMLRPC::Client.new2("http://api.mailchimp.com/1.1/")
-          chimpAPI.call("listSubscribe", apikey, list_id, email, merge_vars, content_type, double_opt)
-        end
-        
-        def chimpUnsubscribe(apikey, list_id, email, delete_user = false, send_goodbye = true, send_notify = true)
-          raise_errors(apikey, list_id)
-          chimpAPI ||= XMLRPC::Client.new2("http://api.mailchimp.com/1.1/")
-          chimpAPI.call("listUnsubscribe", apikey, list_id, email, delete_user, send_goodbye, send_notify)
-        end
-        
-        def chimpUpdate(apikey, list_id, email, merge_vars, content_type = 'html', replace_interests = true)
-          raise_errors(apikey, list_id)
-          chimpAPI ||= XMLRPC::Client.new2("http://api.mailchimp.com/1.1/")
-          chimpAPI.call("listUpdateMember", apikey, list_id, email, merge_vars, content_type, replace_interests)
+          hominid.update_member(list_id["id"], current_email, vars, self[type_column])
+        rescue
+          false
         end
         
         def find_mailing_list(apikey, list_name)
-          raise MailChimpConfigError("Please provide a mailing list name") if list_name.nil?
-          mailing_lists ||= get_all_mailing_lists(apikey)
-          unless mailing_lists.nil?  
-            mailing_lists.find { |list| list["name"] == list_name }
-          end
+          hominid ||= Hominid.new
+          mailing_lists ||= hominid.lists
+          mailing_lists.find {|list| list["name"] == list_name} unless mailing_lists.nil?
         end
-        
-        def get_all_mailing_lists(apikey)
-          raise MailChimpConnectError("Please login to MailChimp and make sure you have a valid API key") if (apikey.nil?) 
-          chimpAPI ||= XMLRPC::Client.new2("http://api.mailchimp.com/1.1/")
-          chimpAPI.call("lists", apikey)
-        end
-        
-        def raise_errors(apikey, list_id)
-          raise MailChimpConnectError("Please login to MailChimp and make sure you have a valid API key") if apikey.nil?
-          raise MailChimpConfigError("Please provide a valid mailing list ID") if list_id.nil?
-        end
-        
       end
     end
   end
